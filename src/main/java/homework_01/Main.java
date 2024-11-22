9d4634214da1587910ac6a662686d969699663a7
SergiusSchmidt <sistemma2011@googlemail.com>
2024-11-22 01:32:36 +0100

    homework 01

---
+++ src/main/java/homework_01/Main.java
@@ -0,0 +1,12 @@
+package homework_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a = 1;
+        int b = 1;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
