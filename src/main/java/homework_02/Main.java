932737c80cd87c491b49c6455d2880c4946ac7a0
SergiusSchmidt <sistemma2011@googlemail.com>
2024-11-26 19:36:35 +0100

    homework 02

---
+++ src/main/java/homework_02/Main.java
@@ -0,0 +1,12 @@
+package homework_02;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int x = 2;
+        int y = 2;
+        int result = x / y;
+        System.out.println("Результата - " + result);
+    }
+}
